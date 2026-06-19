7b0a3d3691cf26344e2d483f0d425e1c62db65b0
Praneeth Guduru <praneeth@Praneeths-MacBook-Air.local>
2026-06-19 22:02:35 +0530

    removed the unwanted prints making the output clean and better

--- src/ml/features_classifier.py
+++ src/ml/features_classifier.py
@@ -6,15 +6,11 @@ def classify_features(df, target_column):
     numerical_columns = []
     categorical_columns = []
 
-    print("\n=== FEATURE CLASSIFICATION ===\n")
-
     for col in df.columns:
 
         # Skip target column
         if col == target_column:
             continue
-
-        print(f"{col} -> {df[col].dtype}")
 
         # Handle string/object columns
         if str(df[col].dtype) in ["object", "str"]:
@@ -29,28 +25,16 @@ def classify_features(df, target_column):
                 / len(df)
             )
 
-            print(
-                f"   Numeric Conversion Success Rate: "
-                f"{success_rate:.2f}"
-            )
-
             if success_rate > 0.90:
                 numerical_columns.append(col)
-                print("   -> Classified as NUMERICAL")
 
             else:
                 categorical_columns.append(col)
-                print("   -> Classified as CATEGORICAL")
 
         # Handle actual numeric columns
         else:
 
             numerical_columns.append(col)
-
-            #print(
-            #    "   -> Classified as NUMERICAL "
-            #    "(native numeric dtype)"
-            #)
 
     return {
         "numerical_columns": numerical_columns,
